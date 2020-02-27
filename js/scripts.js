b54b393810670a2f462e349f6f861a6ed776c2e4
Martha Norrick <martharuthnorrick@gmail.com>
2020-02-27 15:42:11 -0500

    ugh

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ var initOptions = {
   zoom: initialZoom, // initial view zoom level (0-18)
 }
 
-var TreeHealth - (code) => {
+var FindTreeHealth - (code) => {
   switch (code) {
     case 'Fair':
       return {
@@ -41,11 +41,101 @@ var map = new mapboxgl.Map(initOptions);
 // add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
 
-/ wait for the initial style to Load
-map.on('style.load', function() {
+  // wait for the initial style to Load
+  map.on('style.load', function() {
 
-  // add a geojson source to the map using our external geojson file
-  map.addSource('bk_nta_trees', {
-    type: 'geojson',
-    data: './data/bk_nta_trees.geojson',
-  });
+    // add a geojson source to the map using our external geojson file
+    map.addSource('bk_nta_trees', {
+      type: 'geojson',
+      data: './data/bk_nta_tress.geojson',
+    });
+
+    // let's make sure the source got added by logging the current map state to the console
+    console.log(map.getStyle().sources)
+
+    // add a layer for our custom source
+    map.addLayer({
+      id: 'point-bk-nta-trees',
+      type: 'circle',
+      source: 'bk_nta_trees',
+      paint: {
+        'circle-color': {
+          type: 'categorical',
+          property: 'health',
+          stops: [
+            [
+              'Fair',
+              FindTreeHealth('Fair').color,
+            ],
+            [
+              'Good',
+              FindTreeHealth('Good').color,
+            ],
+            [
+              'Poor',
+              FindTreeHealth('Poor').color,
+            ],
+          ]
+        }
+      }
+    })
+
+    // add an empty data source, which we will use to highlight the lot the user is hovering over
+    map.addSource('highlight-feature', {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: []
+      }
+    })
+
+    // add a layer for the highlighted lot
+    map.addLayer({
+      id: 'highlight-line',
+      type: 'circle',
+      source: 'highlight-feature',
+      paint: {
+        'circle-stroke-width': 0.1,
+        'circle-stroke-color': 'white',
+      }
+    });
+
+    // listen for the mouse moving over the map and react when the cursor is over our data
+
+    map.on('mousemove', function (e) {
+      // query for the features under the mouse, but only in the lots layer
+      var features = map.queryRenderedFeatures(e.point, {
+          layers: ['point-bk_nta_trees'],
+      });
+
+      // if the mouse pointer is over a feature on our layer of interest
+      // take the data for that feature and display it in the sidebar
+      if (features.length > 0) {
+        map.getCanvas().style.cursor = 'pointer';  // make the cursor a pointer
+
+        var hoveredFeature = features[0]
+        var featureInfo = `
+          <h4>${hoveredFeature.properties.Address}</h4>
+          <p><strong>Land Use:</strong> ${LandUseLookup(parseInt(hoveredFeature.properties.LandUse)).description}</p>
+          <p><strong>Zoning:</strong> ${hoveredFeature.properties.ZoneDist1}</p>
+        `
+        $('#feature-info').html(featureInfo)
+
+        // set this lot's polygon feature as the data for the highlight source
+        map.getSource('highlight-feature').setData(hoveredFeature.geometry);
+      } else {
+        // if there is no feature under the mouse, reset things:
+        map.getCanvas().style.cursor = 'default'; // make the cursor default
+
+        // reset the highlight source to an empty featurecollection
+        map.getSource('highlight-feature').setData({
+          type: 'FeatureCollection',
+          features: []
+        });
+
+        // reset the default message
+        $('#feature-info').html(defaultText)
+      }
+    })
+
+  })
